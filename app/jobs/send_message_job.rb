d591dc72b0475c186fa9f1bcf1da9d683c27c3a7
Provat Sutradhar <s.provatcsd01@gmail.com>
2021-09-25 21:34:13 +0600

    modified file for chatting

--- app/jobs/send_message_job.rb
+++ app/jobs/send_message_job.rb
@@ -2,12 +2,17 @@ class SendMessageJob < ApplicationJob
   queue_as :default
 
   def perform(message)
+      mine = ApplicationController.render(
+       partial: 'messages/mine',
+       locals: {message: message}
+       )
 
-         html = ApplicationController.render(
-           partial: 'messages/message',
-           locals: {message: message}
-             )
-         ActionCable.server.broadcast
-         "room_channel_#{message.room_id}", html: html
+       theirs = ApplicationController.render(
+        partial: 'messages/theirs',
+        locals: {message: message}
+        )
+     ActionCable.server.broadcast "room_channel_#{message.room_id}",
+      mine: mine, theirs: theirs, message: message
+
   end
 end
